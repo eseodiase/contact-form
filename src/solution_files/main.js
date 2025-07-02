f9fd1827e799be824f72c2cf8a7e19a2e5249313
eseodiase <reacheseodiase@gmail.com>
2025-07-02 21:02:51 +0100

    update

--- src/solution_files/main.js
+++ src/solution_files/main.js
@@ -1,5 +1,22 @@
 function contactForm() {
-  // Tab to edit
+  // variable
   const fn = document.getElementById("fname");
   const ln = document.getElementById("lname");
-}
+  const em = document.getElementById("email");
+  const gnr = document.getElementById("general");
+  const sp = document.getElementById("support");
+  const ms = document.getElementById("message");
+  const ck = document.getElementById("check");
+  const sum = document.getElementById("submit");
+  const fm = document.getElementById("form");
+  
+  //
+  sum.addEventListener('click', () => {
+    if (fn.value.trim() === "") {
+      document.getElementById('fname_error').innerHTML = "This field is required";
+    } else {
+      document.getElementById('fname_error').innerHTML = ""; // clear error
+    }
+  });
+  }
+  contactForm();
