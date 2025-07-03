eb046722010167a4dcd4be1ea352e2038db67ae6
eseodiase <reacheseodiase@gmail.com>
2025-07-03 15:31:36 +0100

    success message now looking good

--- src/main.js
+++ src/main.js
@@ -12,10 +12,12 @@ function contactForm() {
       e.preventDefault()
     } 
     else {
-    document.getElementById('success').innerHTML = `<div>
-      <p>Message Sent!</p>
-  <p>Thanks for completing the form. We ll be in touch soon!</p>
-  </div>`;
+    document.getElementById('success').innerHTML = `    <div class="success_inner">
+    <p class="success-text-1">
+      <span class="success-icon"></span>
+      Message Sent!</p>
+    <p class="success-text-2">Thanks for completing the form. We'll be in touch soon!</p>
+    </div>`;
   sus.style.display = "block";
     }
 })
