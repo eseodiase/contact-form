0808148ac483f370bf6f1b07d1e48a7a933bb687
eseodiase <reacheseodiase@gmail.com>
2025-07-03 12:24:31 +0100

    success state section needs to be centered

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ function contactForm() {
   const sum = document.getElementById("submit");
   const fn = document.getElementById("fname");
   
+  const sus = document.getElementById("success");
+  
   
   sum.addEventListener('click', (e) => {
     if (fn.value === "") {
       document.getElementById('fname_error').innerHTML = "This field is required";
       e.preventDefault()
     } 
+    else {
+    document.getElementById('success').innerHTML = `<div>
+      <p>Message Sent!</p>
+  <p>Thanks for completing the form. We ll be in touch soon!</p>
+  </div>`;
+  sus.style.display = "block";
+    }
 })
   };
 contactForm();
