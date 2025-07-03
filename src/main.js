da793ccd94f50da82c515b6f235a2ff8f5ab501f
eseodiase <reacheseodiase@gmail.com>
2025-07-03 19:54:13 +0100

    still working on error conditions

--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
 
 function contactForm() {
   const sum = document.getElementById("submit");
-  const fn = document.getElementById("fname");
-  
-  const sus = document.getElementById("success");
+const fn = document.getElementById("fname");
+const ln = document.getElementById("lname");
+const em = document.getElementById("email");
+const gn = document.getElementById("general");
+const sp = document.getElementById("support");
+const ms = document.getElementById("message");
+const ck = document.getElementById("check");
+const sus = document.getElementById("success");
   
   
   sum.addEventListener('click', (e) => {
-    if (fn.value === "") {
-      document.getElementById('fname_error').innerHTML = "This field is required";
-      e.preventDefault()
-    } 
+    if (
+  fn.value === "" ||
+  ln.value === "" ||
+  em.value === "" ||
+  (gn.checked === false &&
+   sp.checked === false)||
+  ms.value === "" ||
+  ck.checked === false
+) {
+  e.preventDefault();
+}
     else {
     document.getElementById('success').innerHTML = `    <div class="success_inner">
     <p class="success-text-1">
@@ -19,7 +31,10 @@ function contactForm() {
     <p class="success-text-2">Thanks for completing the form. We'll be in touch soon!</p>
     </div>`;
   sus.style.display = "block";
+  document.querySelector('.wrapper').style.marginTop = "7.8rem";
     }
 })
+
+
   };
 contactForm();
