94ed476ac90df6fd8acf857d9dc39989cf24be64
eseodiase <reacheseodiase@gmail.com>
2025-07-03 11:22:57 +0100

    preventing default submitting

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ function contactForm() {
   const sum = document.getElementById("submit");
   const fn = document.getElementById("fname");
   
-  sum.addEventListener('click', () => {
-    if (fn.value.trim() === "") {
-      document.getElementById('fname_error').innerHTML = "say error";
-    } else {
-      document.getElementById('fname_error').innerHTML = ""; // clear error
-    }
-  });
-}
+  
+  sum.addEventListener('click', (e) => {
+    if (fn.value === "") {
+      document.getElementById('fname_error').innerHTML = "This field is required";
+      e.preventDefault()
+    } 
+})
+  };
 contactForm();
